a461663b8d1aa1da58aada7b670f033df5473b39
mmkamal021 <114946934+mmkamal021@users.noreply.github.com>
2024-02-11 23:34:13 +0600

    Concatenations of JS

---
+++ script_concatenation.js
@@ -0,0 +1,12 @@
+var text = `how ` + `are u ?`;
+var welcomeMsg = `Hello,` + text;
+
+console.log(welcomeMsg);
+console.log(typeof welcomeMsg);
+
+var s = 20 + "30";
+console.log(s);
+console.log(typeof s);
+
+var complex = 10 + 10 + `some ` + 10;
+console.log(complex);
